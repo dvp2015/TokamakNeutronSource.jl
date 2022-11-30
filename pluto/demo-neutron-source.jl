b7d99ef4f17806bbbad4b0e0f341761a072557a6
dvp2015 <dmitri_portnov@yahoo.com>
2022-11-30 19:44:08 +0300

    pluto: check integration

--- pluto/demo-neutron-source.jl
+++ pluto/demo-neutron-source.jl
@@ -27,9 +27,6 @@ using Cuba, GLMakie
 # ╔═╡ e8552241-d48d-4469-abae-484db725af36
 using TokamakNeutronSource.PlasmaDistribution
 
-# ╔═╡ e45204a1-a4d7-4055-ad12-274c8287e048
-Pkg.installed()
-
 # ╔═╡ 5f37ce53-477f-498d-b1ea-613fae26f3f8
 begin
 	eqdsk_path = joinpath(tns_pkg_dir, "test", "data", "beforeTQ.eqdsk")
@@ -50,15 +47,15 @@ distr = DDDistribution(eqdsk, excel)
 begin
 	r = rpoints(eqdsk)
 	z = zpoints(eqdsk)
-	I = [ Idd(distr, ri, zi) for ri in r, zi in z] 	
-	extrema(I)
+	_I = I(distr, r, z)
+	extrema(_I)
 end
 
 # ╔═╡ 5d1b8d4f-bccd-43c1-81ff-4c186b3b5367
 typeof(I)
 
 # ╔═╡ e7b4f8b3-f3f0-4d5c-9080-eea93e01dd17
-function plot_dd_source(eqdsk::Content, distr::Distribution)
+function plot_neutron_source(eqdsk::Content, distr::AbstractDistribution)
 	f = Figure(resolution=(600, 800))
 	ax = Axis(
 		f[1,1]; 
@@ -71,7 +68,7 @@ function plot_dd_source(eqdsk::Content, distr::Distribution)
 	z = zpoints(eqdsk)
 	xlims!(ax, r[1], r[end])
 	ylims!(ax, z[1], z[end])
-	_I = [ I(distr, ri, zi) for ri in r, zi in z] 	
+	_I = I(distr, r, z)
 	levels=range(0, 1.3e10, length=14)
 	cntr = contourf!(ax,
 		r, z, _I,
@@ -96,61 +93,74 @@ function plot_dd_source(eqdsk::Content, distr::Distribution)
 end
 
 # ╔═╡ 6888f022-b9dc-43f6-8759-772f97afc734
-plot_dd_source(eqdsk, distr)
-
-# ╔═╡ f1500735-efde-446b-9c28-734930662d7d
-a = distr.ψ([2, 2.1],0.5)
-
-
-# ╔═╡ ebb2f1ab-5875-44cd-b9fd-6fa53f86d541
-distr.n.(a)
+plot_neutron_source(eqdsk, distr)
 
 # ╔═╡ f453daec-8564-48e4-8869-834d3ef64fd7
 
 
 # ╔═╡ 2c9df88e-b0a1-4078-a539-685c3c85f676
-function compute_total_output(distr::Distribution, I::Function)
+function compute_total_output(distr, I::Function)
 	function scaler(a, b)
 		dx = b - a
-		x -> dx .* x .+ a
+		x -> dx * x + a
 	end
 	rscaler = scaler(distr.rmin, distr.rmax)
 	zscaler = scaler(distr.zmin, distr.zmax)	
-	integrand(x, f) = f[1] = x[1]*I(rscaler(x[1]), zscaler(x[2]))
-	# cuhre(integrand)
-	integral, error, probability, neval, fail, nregions = cuhre(integrand)
-	# Normalization:
-	# 2π - integral over torroidal direction, 
-	# (...)(...) - area of R,Z integration domain, square meters
-	# 1e6 - m^3 -> cm^3
-	normalization = 2π*(distr.rmax-distr.rmin)*(distr.zmax - distr.zmin)*1e6
-	integral*normalization, error*normalization, probability, neval, fail, nregions
+	function integrand(x, f) 
+		println("x=", x)
+		f[1] = x[1]*I(rscaler(x[1]), zscaler(x[2]))
+		println("f=", f)
+	end
+	cuhre(integrand)
+	# integral, error, probability, neval, fail, nregions = cuhre(integrand)
+	# # Normalization:
+	# # 2π - integral over torroidal direction, 
+	# # (...)(...) - area of R,Z integration domain, square meters
+	# # 1e6 - m^3 -> cm^3
+	# normalization = 2π*(distr.rmax-distr.rmin)*(distr.zmax - distr.zmin)*1e6
+	# integral*normalization, error*normalization, probability, neval, fail, nregions
 end
 
 # ╔═╡ a3e884fe-362a-4be6-aba6-6cd33da663ce
 let
-	_I(r,z)= I(distr, r, z)
+	function _I(r,z)
+		println("r= ", r, ", z= ", z)
+		I(distr, r, z)
+	end
 	compute_total_output(distr, _I)
 end
 
 # ╔═╡ a49ac38f-c3b9-4775-b845-939c9d0f8834
-function compute_total_output_vectorized(distr::Distribution, I::Function)
+function compute_total_output_vectorized(distr::DDDistribution, I::Function)
 	function scaler(a, b)
 		dx = b - a
-		x -> dx .* x .+ a
+		x -> (dx .* x) .+ a
 	end
 	rscaler = scaler(distr.rmin, distr.rmax)
 	zscaler = scaler(distr.zmin, distr.zmax)	
-	integrand(x, f) = f[1,:] = x[1,:]*I(rscaler(x[1,:]), zscaler(x[2,:]))
-	cuhre(integrand; nvec=10)
+	function integrand(x, f) 
+		println("x=", x)
+		f[:] .= [r*I(r, z) for (r,z) in zip(rscaler(x[1,:]), zscaler(x[2,:]))]
+		println("f=", f)
+	end
+	cuhre(integrand, nvec=1)
 	# integral, error, probability, neval, fail, nregions = cuhre(integrand)
 	# normalization = 2π*(distr.rmax-distr.rmin)*(distr.zmax - distr.zmin)*10000
 	# integral*normalization, error*normalization, probability, neval, fail, nregions
 end
 
+# ╔═╡ 176f455b-bac2-402b-bc9f-e2ae80df5477
+let
+	function _I(r,z)
+		println("r= ", r, ", z= ", z)
+		I(distr, r, z)
+	end
+	compute_total_output_vectorized(distr, _I)
+	# TODO dvp: check why the results differ.
+end
+
 # ╔═╡ Cell order:
 # ╠═26433bb2-6ff1-11ed-0943-01e79517b4f5
-# ╠═e45204a1-a4d7-4055-ad12-274c8287e048
 # ╠═9597cccc-5f54-4260-bf2e-2fcf4f3a169a
 # ╠═5f37ce53-477f-498d-b1ea-613fae26f3f8
 # ╠═50b9960f-7673-4975-b24b-d96ebb16d50f
@@ -161,9 +171,8 @@ end
 # ╠═5d1b8d4f-bccd-43c1-81ff-4c186b3b5367
 # ╠═e7b4f8b3-f3f0-4d5c-9080-eea93e01dd17
 # ╠═6888f022-b9dc-43f6-8759-772f97afc734
-# ╠═f1500735-efde-446b-9c28-734930662d7d
-# ╠═ebb2f1ab-5875-44cd-b9fd-6fa53f86d541
 # ╠═f453daec-8564-48e4-8869-834d3ef64fd7
 # ╠═2c9df88e-b0a1-4078-a539-685c3c85f676
 # ╠═a3e884fe-362a-4be6-aba6-6cd33da663ce
 # ╠═a49ac38f-c3b9-4775-b845-939c9d0f8834
+# ╠═176f455b-bac2-402b-bc9f-e2ae80df5477
